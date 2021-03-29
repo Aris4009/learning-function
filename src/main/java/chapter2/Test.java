aced1c2eafa19b78da275171c738aacdd0314cc9
he <slut@vip.qq.com>
2021-03-29 16:06:56 +0800

    chapter2

---
+++ src/main/java/chapter2/Test.java
@@ -0,0 +1,33 @@
+package chapter2;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.function.Supplier;
+
+import javax.swing.text.DateFormatter;
+
+public class Test {
+
+	public static void main(String[] args) {
+		DateFormat dateFormat = new SimpleDateFormat("dd-MMM-yyyy");
+		DateFormat dateFormat1 = new SimpleDateFormat("yyyy-MM-dd");
+		List<DateFormat> list = new ArrayList<>();
+		list.add(dateFormat);
+		list.add(dateFormat1);
+
+		Supplier<DateFormatter> dateFormatSupplier = DateFormatter::new;
+		ThreadLocal<DateFormatter> threadLocal = ThreadLocal.withInitial(dateFormatSupplier);
+
+		Date date = new Date();
+		for (int i = 0; i < list.size(); i++) {
+			final DateFormat d = list.get(i);
+			new Thread(() -> {
+				threadLocal.get().setFormat(d);
+				System.out.println(d.format(date));
+			}).start();
+		}
+	}
+}
